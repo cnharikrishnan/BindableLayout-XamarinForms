35fa3bf53e4af672766fe3778dd202c3b165f6d0
Harikrishnan <cnharishkrish@gmail.com>
2019-11-22 00:19:26 +0530

    Sample, Screenshots and documentation is committed.

---
+++ BindableLayout/BindableLayout/BindableLayout/ViewModel/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BindableLayout
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            this.GetContactsList();
+        }
+
+        public List<PlatformInfo> PlatformsList { get; set; }
+
+        private void GetContactsList()
+        {
+            if (this.PlatformsList == null)
+                this.PlatformsList = new List<PlatformInfo>();
+
+            this.PlatformsList.Add(new PlatformInfo() { IsChecked = true, PlatformName = "Android" });
+            this.PlatformsList.Add(new PlatformInfo() { IsChecked = true, PlatformName = "iOS" });
+            this.PlatformsList.Add(new PlatformInfo() { IsChecked = false, PlatformName = "UWP" });
+        }
+    }
+}
